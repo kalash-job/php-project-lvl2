26f163378ddd92b20d317601d1cb92832e0d7be9
Nikolay Kalashnikov <kalash-job@yandex.ru>
2020-05-17 22:48:07 +0300

    Add new tests in tests/DiffTest.php

--- tests/DiffTest.php
+++ tests/DiffTest.php
@@ -16,9 +16,9 @@ class DiffTest extends TestCase
             'proxy' => '123.234.53.22'
         ];
         $after = [
-            'host' => 'hexlet.io',
-            'timeout' => 50,
-            'proxy' => '123.234.53.22'
+            'timeout' => 20,
+            'verbose' => true,
+            'host' => 'hexlet.io'
         ];
         $expected = [
             '  host: hexlet.io',
@@ -26,6 +26,48 @@ class DiffTest extends TestCase
             '- timeout: 50',
             '- proxy: 123.234.53.22',
             '+ verbose: true'
+        ];
+        $actual = getDiff($before, $after);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testGetDiffWithEmptyValue()
+    {
+        $before = [
+            'host' => 'hexlet.io',
+            'timeout' => 50,
+            'proxy' => '123.234.53.22'
+        ];
+        $after = [];
+        $expected = [
+            '- host: hexlet.io',
+            '- timeout: 50',
+            '- proxy: 123.234.53.22'
+        ];
+        $actual = getDiff($before, $after);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testGetDiffWithBooleanValue()
+    {
+        $before = [
+            'host' => 'hexlet.io',
+            'timeout' => 50,
+            'proxy' => '123.234.53.22'
+        ];
+        $after = [
+            'timeout' => 0,
+            'verbose' => true,
+            'host' => 'hexlet.io',
+            'result' => false
+        ];
+        $expected = [
+            '  host: hexlet.io',
+            '+ timeout: 0',
+            '- timeout: 50',
+            '- proxy: 123.234.53.22',
+            '+ verbose: true',
+            '+ result: false'
         ];
         $actual = getDiff($before, $after);
         $this->assertEquals($expected, $actual);
