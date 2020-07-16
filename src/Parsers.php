ccc8ec2b18977a2feef764bd4c332265c40acf2a
Nikolay Kalashnikov <kalash-job@yandex.ru>
2020-07-16 20:44:36 +0300

    Add parametr JSON_THROW_ON_ERROR to json_decode in Parsers.php

--- src/Parsers.php
+++ src/Parsers.php
@@ -6,34 +6,7 @@ use Symfony\Component\Yaml\Yaml;
 
 function parseJson(string $jsonData)
 {
-    $data = json_decode($jsonData);
-    switch (json_last_error()) {
-        case JSON_ERROR_NONE:
-            $data_error  = '';
-            break;
-        case JSON_ERROR_DEPTH:
-            $data_error  = " - Maximum stack depth exceeded\n";
-            break;
-        case JSON_ERROR_STATE_MISMATCH:
-            $data_error  = " - Underflow or the modes mismatch\n";
-            break;
-        case JSON_ERROR_CTRL_CHAR:
-            $data_error  = " - Unexpected control character found\n";
-            break;
-        case JSON_ERROR_SYNTAX:
-            $data_error  = " - Syntax error, malformed JSON\n";
-            break;
-        case JSON_ERROR_UTF8:
-            $data_error  = " - Malformed UTF-8 characters, possibly incorrectly encoded\n";
-            break;
-        default:
-            $data_error  = " - Unknown error\n";
-            break;
-    }
-    if ($data_error !== '') {
-        throw new \Exception("Error of JSON encoding: {$data_error}\n");
-    }
-    return $data;
+    return json_decode($jsonData, false, 512,  JSON_THROW_ON_ERROR);
 }
 
 function parseYaml(string $yamlData)
