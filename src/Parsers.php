df9e6fd204c0e1e0d202a7650279a924477730d3
Nikolay Kalashnikov <kalash-job@yandex.ru>
2020-07-23 21:37:55 +0300

    To correct names of variables and message of exception

--- src/Parsers.php
+++ src/Parsers.php
@@ -4,13 +4,13 @@ namespace Differ\Parsers;
 
 use Symfony\Component\Yaml\Yaml;
 
-function parse(string $filesContent, $filesFormat)
+function parse(string $content, $format)
 {
-    if ($filesFormat === 'json') {
-        return json_decode($filesContent, false, 512, JSON_THROW_ON_ERROR);
-    } elseif ($filesFormat === 'yaml' || $filesFormat === 'yml') {
-        return Yaml::parse($filesContent, Yaml::PARSE_OBJECT_FOR_MAP);
+    if ($format === 'json') {
+        return json_decode($content, false, 512, JSON_THROW_ON_ERROR);
+    } elseif ($format === 'yaml' || $format === 'yml') {
+        return Yaml::parse($content, Yaml::PARSE_OBJECT_FOR_MAP);
     } else {
-        throw new \Exception("File $path must be in JSON or YAML format\n");
+        throw new \Exception("File must be in JSON or YAML format");
     }
 }
