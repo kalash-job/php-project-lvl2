c2a7ea0aa5f66c4308e734982e559d0304d0fdfe
Nikolay Kalashnikov <kalash-job@yandex.ru>
2020-08-06 06:04:56 +0300

    To change array's returns to string's returns in renderPlainDiff function

--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -2,6 +2,8 @@
 
 namespace Differ\Plain;
 
+use function Functional\flatten;
+
 function stringify($value)
 {
     if ($value === true) {
@@ -18,18 +20,18 @@ function stringify($value)
     return $value;
 }
 
-function iter($node, string $path): array
+function iter($node, string $path)
 {
     switch ($node['type']) {
         case 'changed':
             $oldValue = stringify($node['oldValue']);
             $newValue = stringify($node['newValue']);
-            return ["Property '{$path}{$node['key']}' was changed. From {$oldValue} to {$newValue}"];
+            return "Property '{$path}{$node['key']}' was changed. From {$oldValue} to {$newValue}";
         case 'added':
             $value = stringify($node['value']);
-            return ["Property '{$path}{$node['key']}' was added with value: {$value}"];
+            return "Property '{$path}{$node['key']}' was added with value: {$value}";
         case 'removed':
-            return ["Property '{$path}{$node['key']}' was removed"];
+            return "Property '{$path}{$node['key']}' was removed";
         case 'same':
             return [];
         case 'parent':
@@ -39,16 +41,17 @@ function iter($node, string $path): array
         default:
             throw new \Exception("Unknown node type '{$node['type']}'");
     }
-    return array_reduce($children, function ($acc, $child) use ($newPath) {
-        return array_merge($acc, iter($child, $newPath));
-    }, []);
+    return array_map(function ($child) use ($newPath) {
+        return iter($child, $newPath);
+    }, $children);
 }
 
 function renderPlainDiff(array $diff): string
 {
-    $lines = array_reduce($diff, function ($acc, $node) {
-        return array_merge($acc, iter($node, ''));
-    }, []);
-    $result = implode("\n", $lines);
+    $lines = array_map(function ($node) {
+        return iter($node, '');
+    }, $diff);
+    $linesFlattened = flatten($lines);
+    $result = implode("\n", $linesFlattened);
     return "{$result}\n";
 }
