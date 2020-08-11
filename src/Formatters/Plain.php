4091b0e942421daa3b08df6172a5448a2b32da19
Nikolay Kalashnikov <kalash-job@yandex.ru>
2020-08-11 06:58:29 +0300

    Build correct structure of rendering functions

--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -20,36 +20,34 @@ function stringify($value)
     return $value;
 }
 
-function iter($node, string $path)
+function iter(array $tree, string $path): array
 {
-    switch ($node['type']) {
-        case 'changed':
-            $oldValue = stringify($node['oldValue']);
-            $newValue = stringify($node['newValue']);
-            return "Property '{$path}{$node['key']}' was changed. From {$oldValue} to {$newValue}";
-        case 'added':
-            $value = stringify($node['value']);
-            return "Property '{$path}{$node['key']}' was added with value: {$value}";
-        case 'removed':
-            return "Property '{$path}{$node['key']}' was removed";
-        case 'same':
-            return [];
-        case 'parent':
-            $newPath = "{$path}{$node['key']}.";
-            $children = $node['children'];
-            return array_map(function ($child) use ($newPath) {
-                return iter($child, $newPath);
-            }, $children);
-        default:
-            throw new \Exception("Unknown node type '{$node['type']}'");
-    }
+    return array_map(function ($node) use ($path) {
+        switch ($node['type']) {
+            case 'changed':
+                $oldValue = stringify($node['oldValue']);
+                $newValue = stringify($node['newValue']);
+                return "Property '{$path}{$node['key']}' was changed. From {$oldValue} to {$newValue}";
+            case 'added':
+                $value = stringify($node['value']);
+                return "Property '{$path}{$node['key']}' was added with value: {$value}";
+            case 'removed':
+                return "Property '{$path}{$node['key']}' was removed";
+            case 'same':
+                return [];
+            case 'parent':
+                $newPath = "{$path}{$node['key']}.";
+                $children = $node['children'];
+                return iter($children, $newPath);
+            default:
+                throw new \Exception("Unknown node type '{$node['type']}'");
+        }
+    }, $tree);
 }
 
 function renderPlainDiff(array $diff): string
 {
-    $lines = array_map(function ($node) {
-        return iter($node, '');
-    }, $diff);
+    $lines = iter($diff, '');
     $linesFlattened = flatten($lines);
     $result = implode("\n", $linesFlattened);
     return "{$result}\n";
--- src/Formatters/Pretty.php
+++ src/Formatters/Pretty.php
@@ -42,52 +42,52 @@ function stringify($value, int $depth): string
     return "{\n{$unionLine}\n{$lastOffset}    }";
 }
 
-function iter($node, int $depth)
+function iter(array $tree, int $depth): array
 {
-    $prefixMinus = '  - ';
-    $prefixPlus = '  + ';
-    $prefixSpaces = '    ';
-    $offset = str_repeat('    ', $depth - 1);
-    switch ($node['type']) {
-        case 'same':
-            $prefix = $prefixSpaces;
-            $value = stringify($node['value'], $depth);
-            return "{$offset}{$prefix}{$node['key']}: {$value}";
-        case 'added':
-            $prefix = $prefixPlus;
-            $value = stringify($node['value'], $depth);
-            return "{$offset}{$prefix}{$node['key']}: {$value}";
-        case 'removed':
-            $prefix = $prefixMinus;
-            $value = stringify($node['value'], $depth);
-            return "{$offset}{$prefix}{$node['key']}: {$value}";
-        case 'changed':
-            $newValue = stringify($node['newValue'], $depth);
-            $oldValue = stringify($node['oldValue'], $depth);
-            $firstLine = "{$offset}{$prefixPlus}{$node['key']}: {$newValue}";
-            $secondLine = "{$offset}{$prefixMinus}{$node['key']}: {$oldValue}";
-            return "$firstLine\n$secondLine";
-        case 'parent':
-            $prefix = $prefixSpaces;
-            $children = $node['children'];
-            $firstLine = "{$offset}{$prefix}{$node['key']}: {";
-            $newLines = array_map(function ($child) use ($depth) {
-                return iter($child, $depth + 1);
-            }, $children);
-            return array_merge([$firstLine], $newLines, ["{$offset}    }"]);
-        default:
-            throw new \Exception("Unknown node type '{$node['type']}'");
-    }
+    return array_map(function ($node) use ($depth) {
+        $prefixMinus = '  - ';
+        $prefixPlus = '  + ';
+        $prefixSpaces = '    ';
+        $offset = str_repeat('    ', $depth - 1);
+        switch ($node['type']) {
+            case 'same':
+                $prefix = $prefixSpaces;
+                $value = stringify($node['value'], $depth);
+                return "{$offset}{$prefix}{$node['key']}: {$value}";
+            case 'added':
+                $prefix = $prefixPlus;
+                $value = stringify($node['value'], $depth);
+                return "{$offset}{$prefix}{$node['key']}: {$value}";
+            case 'removed':
+                $prefix = $prefixMinus;
+                $value = stringify($node['value'], $depth);
+                return "{$offset}{$prefix}{$node['key']}: {$value}";
+            case 'changed':
+                $newValue = stringify($node['newValue'], $depth);
+                $oldValue = stringify($node['oldValue'], $depth);
+                $firstLine = "{$offset}{$prefixPlus}{$node['key']}: {$newValue}";
+                $secondLine = "{$offset}{$prefixMinus}{$node['key']}: {$oldValue}";
+                return "$firstLine\n$secondLine";
+            case 'parent':
+                $prefix = $prefixSpaces;
+                $children = $node['children'];
+                $firstLine = "{$offset}{$prefix}{$node['key']}: {";
+                $newLines = iter($children, $depth + 1);
+                $lineFromChildren = implode("\n", $newLines);
+                $lastLine = "{$offset}    }";
+                return "{$firstLine}\n{$lineFromChildren}\n{$lastLine}";
+            default:
+                throw new \Exception("Unknown node type '{$node['type']}'");
+        }
+    }, $tree);
 }
 
 function renderDiff(array $diff): string
 {
     $startDepth = 1;
-    $startLine = ["{"];
-    $lastLine = ["}\n"];
-    $lines = array_map(function ($node) use ($startDepth) {
-        return iter($node, $startDepth);
-    }, $diff);
-    $linesFlattened = flatten($lines);
-    return implode("\n", array_merge($startLine, $linesFlattened, $lastLine));
+    $startLine = "{";
+    $lastLine = "}\n";
+    $lines = iter($diff, $startDepth);
+    $joinedLine = implode("\n", flatten($lines));
+    return "{$startLine}\n{$joinedLine}\n{$lastLine}";
 }
